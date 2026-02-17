e7fafed3d6809ac16ef6f49b57e5c79d4eebe705
Nurunnabi87 <nurunnabi.jewel87@gmail.com>
2026-02-17 15:29:27 +0530

    project setup and header section is done

---
+++ app.js
@@ -0,0 +1,9 @@
+// ----------------------
+// Toggle Mobile Menu
+// ----------------------
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("mobile-menu");
+
+btn?.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
